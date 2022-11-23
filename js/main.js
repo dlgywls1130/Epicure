c5ee5b9435779744269c577369f1e42e3f1a53f7
Hyojin <dlgywls1130@gmail.net>
2022-11-24 00:04:30 +0900

    메뉴바 제작중

--- js/main.js
+++ js/main.js
@@ -17,18 +17,21 @@ const tabItem = document.querySelectorAll(".tab-container__item");
 const tabContent = document.querySelectorAll(".content-container__content");
 
 tabItem.forEach((item) => {
-  item.addEventListener("click", tabHandler);
+    item.addEventListener("click", tabHandler);
 });
 
 function tabHandler(item) {
-  const tabTarget = item.currentTarget;
-  const target = tabTarget.dataset.tab;
-  tabItem.forEach((title) => {
-    title.classList.remove("active");
-  });
-  tabContent.forEach((target) => {
-    target.classList.remove("target");
-  });
-  document.querySelector("#" + target).classList.add("target");
-  tabTarget.classList.add("active");
+    const tabTarget = item.currentTarget;
+    const target = tabTarget.dataset.tab;
+    tabItem.forEach((title) => {
+        title.classList.remove("active");
+    });
+    tabContent.forEach((target) => {
+        target.classList.remove("target");
+    });
+    document.querySelector("#" + target).classList.add("target");
+    tabTarget.classList.add("active");
 }
+
+
+//menu tab
