bc3b5f88bb47e3c2cf0084b14c8fb479899d97d8
Hyojin <dlgywls1130@gmail.net>
2022-12-18 01:55:14 +0900

    식당 디테일 페이지 수정중

--- js/filter.js
+++ js/filter.js
@@ -1,27 +1,26 @@
-window.onload = function () {
-    const workBtnContainer = document.querySelector('.bookmark_filter');
-    const projectConainer = document.querySelector('.filter_wrappers');
-    const projects = document.querySelectorAll('.filter_body');
 
-    workBtnContainer.addEventListener('click', (e) => {
-        const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-        if (filter == null) {
-            return;
+const workBtnContainer = document.querySelector('.bookmark_filter');
+const projectConainer = document.querySelector('.filter_wrappers');
+const projects = document.querySelectorAll('.filter_body');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    //remove seleciton
+    const active = document.querySelector('.filter_item.active');
+    active.classList.remove('active');
+    e.target.classList.add('active');
+
+
+    projects.forEach((project) => {
+        if (filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
         }
-
-        //remove seleciton
-        const active = document.querySelector('.filter_item.active');
-        active.classList.remove('active');
-        e.target.classList.add('active');
-
-
-        projects.forEach((project) => {
-            if (filter === project.dataset.type) {
-                project.classList.remove('invisible');
-            } else {
-                project.classList.add('invisible');
-            }
-        });
-
     });
-}
+
+});
